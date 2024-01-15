c9a5f7a710b9096c4d9292be09f3ead1f2a8e740
dk_Edu <pivottocoding@gmail.com>
2024-01-15 17:10:17 +0900

    [Feat] delete all 기능 수정 Jpql을 통한 N+1 문제 해결 및 UserId를 받아 해당 유저의 장바구니 전체 삭제

--- src/main/java/com/example/lonua/cart/repository/querydsl/CartRepositoryCustom.java
+++ src/main/java/com/example/lonua/cart/repository/querydsl/CartRepositoryCustom.java
@@ -1,6 +1,7 @@
 package com.example.lonua.cart.repository.querydsl;
 
 import com.example.lonua.cart.model.entity.Cart;
+import com.example.lonua.common.BaseRes;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
@@ -8,4 +9,5 @@ import java.util.List;
 
 public interface CartRepositoryCustom {
     public Page<Cart> findList(Pageable pageable, Integer userIdx);
+
 }
