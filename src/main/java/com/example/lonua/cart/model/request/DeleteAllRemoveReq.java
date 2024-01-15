d296046c45c59a9751ca85ecdd4fffc9a6d5ddeb
dk <pivottocoding@gmail.com>
2024-01-15 22:09:25 +0900

    [Feat] Swagger 및 Valid 기능 추가 Swagger 및 입력값 검증 기능 추가

--- src/main/java/com/example/lonua/cart/model/request/DeleteAllRemoveReq.java
+++ src/main/java/com/example/lonua/cart/model/request/DeleteAllRemoveReq.java
@@ -1,14 +1,21 @@
 package com.example.lonua.cart.model.request;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 @Data
 public class DeleteAllRemoveReq {
+    @NotNull
+    @Min(value = 1)
+    @ApiModelProperty(value = "userIdx", example = "1", required = true)
     private Integer userIdx;
 }
