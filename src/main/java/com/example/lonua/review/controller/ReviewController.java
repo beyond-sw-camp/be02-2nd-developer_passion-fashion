7859bb5d28e9bef633e597add1fa126f1ea5204d
thanks9807 <thanks9807@naver.com>
2024-01-15 10:10:52 +0900

    [Fix] review swagger api 적용

--- src/main/java/com/example/lonua/review/controller/ReviewController.java
+++ src/main/java/com/example/lonua/review/controller/ReviewController.java
@@ -4,6 +4,8 @@ package com.example.lonua.review.controller;
 import com.example.lonua.config.BaseRes;
 import com.example.lonua.review.model.request.PostReviewReq;
 import com.example.lonua.review.service.ReviewService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,27 +18,27 @@ import org.springframework.web.bind.annotation.RestController;
 public class ReviewController {
 
     private final ReviewService reviewService;
-
+    @ApiOperation(value = "리뷰 등록")
     @RequestMapping(method = RequestMethod.POST, value = "/register")
-    public ResponseEntity readReview(PostReviewReq request) {
+    public ResponseEntity register(PostReviewReq request) {
         BaseRes baseRes = reviewService.registerReview(request);
         return ResponseEntity.ok().body(baseRes);
     }
-
+    @ApiOperation(value = "리뷰 조회")
     @RequestMapping(method = RequestMethod.GET, value = "/read")
-    public ResponseEntity readReview(Integer reviewIdx) {
+    public ResponseEntity readReview(@ApiParam(value = "리뷰 idx") Integer reviewIdx) {
         BaseRes baseRes = reviewService.readReview(reviewIdx);
         return ResponseEntity.ok().body(baseRes);
     }
-
+    @ApiOperation(value = "리뷰 수정")
     @RequestMapping(method = RequestMethod.PATCH, value = "/update")
     public ResponseEntity updateReview(PostReviewReq request) {
         BaseRes baseRes = reviewService.updateReview(request);
         return ResponseEntity.ok().body(baseRes);
     }
-
+    @ApiOperation(value = "리뷰 삭제")
     @RequestMapping(method = RequestMethod.DELETE, value = "/delete")
-    public ResponseEntity deleteReview(Integer reviewIdx) {
+    public ResponseEntity deleteReview(@ApiParam(value = "리뷰 idx") Integer reviewIdx) {
         BaseRes baseRes =  reviewService.deleteReview(reviewIdx);
         return ResponseEntity.ok().body(baseRes);
     }
--- src/main/java/com/example/lonua/review/model/request/PostReviewReq.java
+++ src/main/java/com/example/lonua/review/model/request/PostReviewReq.java
@@ -2,12 +2,17 @@ package com.example.lonua.review.model.request;
 
 import com.example.lonua.product.model.entity.Product;
 import com.example.lonua.user.model.entity.User;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
 
 import javax.persistence.*;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import java.time.LocalDateTime;
 import java.util.Date;
 
@@ -17,14 +22,37 @@ import java.util.Date;
 @NoArgsConstructor
 @AllArgsConstructor
 public class PostReviewReq {
-
+    @NotNull
+    @Min(value = 1)
+    @ApiModelProperty(value = "리뷰 IDX", example = "1")
     private Integer reviewIdx;
-    private Product product;
-    private User user;
+
+    @NotNull
+    @Min(value = 1)
+    @ApiModelProperty(value = "리뷰 대상 상품 IDX", example = "1")
+    private Integer productIdx;
+
+    @NotNull
+    @Min(value = 1)
+    @ApiModelProperty(value = "리뷰 작성자 IDX", example = "1")
+    private Integer userIdx;
+
+    @NotNull
+    @Length(max = 500)
+    @ApiModelProperty(value = "리뷰 내용", example = "제품이 친절하고 사장님이 맛있어요")
     private String reviewContent;
+
+    //@NotNull
+    //@Length(max = 500)
+    @ApiModelProperty(value = "리뷰 첨부 사진")
     private String reviewPhoto;
+
+    @NotNull
+    @Length(max = 500)
+    @ApiModelProperty(value = "리뷰 평가 점수 내용", example = "제품이 친절하고 사장님이 맛있어요")
     private Integer evaluation;
-    private String createdAt;
-    private String updatedAt;
-    private Boolean status;
+
+    //private String createdAt;
+    //private String updatedAt;
+    //private Boolean status;
 }
