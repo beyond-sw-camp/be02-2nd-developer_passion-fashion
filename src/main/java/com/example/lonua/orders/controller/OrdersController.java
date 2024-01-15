7b3dcc862f406ee8c006be11f6b6bf144c9bf153
hyungdoyou <yhd4286@gmail.com>
2024-01-16 02:13:08 +0900

    [Docs] 주문 API 입력값 검증, 스웨거 추가 [Docs] 주문 API 입력값 검증, 스웨거 추가

--- src/main/java/com/example/lonua/orders/controller/OrdersController.java
+++ src/main/java/com/example/lonua/orders/controller/OrdersController.java
@@ -5,20 +5,34 @@ import com.example.lonua.common.BaseRes;
 import com.example.lonua.orders.model.request.PostCreateOrdersReq;
 import com.example.lonua.orders.service.OrdersService;
 import com.example.lonua.user.model.entity.User;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/orders")
+@Api(value="주문 컨트롤러 v1", tags="주문 API")
 public class OrdersController {
 
     private final OrdersService ordersService;
 
+    @ApiOperation(value = "주문 생성", response = BaseRes.class, notes = "회원이 상품을 주문합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK ( 요청 성공 )", content = { @Content(mediaType = "application/json", schema = @Schema(implementation = BaseRes.class)) }) })
     @RequestMapping(method = RequestMethod.POST, value = "/create")
-    public ResponseEntity create(@RequestBody PostCreateOrdersReq postCreateOrdersReq){
+    public ResponseEntity create(@RequestBody @Valid PostCreateOrdersReq postCreateOrdersReq){
 
         User user = ((User)SecurityContextHolder.getContext().getAuthentication().getPrincipal());
         BaseRes baseRes = ordersService.createOrder(user, postCreateOrdersReq);
@@ -26,24 +40,33 @@ public class OrdersController {
         return ResponseEntity.ok().body(baseRes);
     }
 
+    @ApiOperation(value = "주문 목록 조회", response = BaseRes.class, notes = "회원이 주문한 상품의 목록을 조회합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK ( 요청 성공 )", content = { @Content(mediaType = "application/json", schema = @Schema(implementation = BaseRes.class)) }) })
     @RequestMapping(method = RequestMethod.GET, value = "/list/{page}/{size}")
-    public ResponseEntity list(@PathVariable Integer page, @PathVariable Integer size){
+    public ResponseEntity list(@PathVariable @NotNull @Positive Integer page, @PathVariable @NotNull @Positive Integer size){
         User user = ((User)SecurityContextHolder.getContext().getAuthentication().getPrincipal());
         BaseRes baseRes = ordersService.list(user, page, size);
 
         return ResponseEntity.ok().body(baseRes);
     }
 
+    @ApiOperation(value = "특정 주문 내역 세부 조회", response = BaseRes.class, notes = "회원이 특정 주문 내역의 세부정보를 조회합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK ( 요청 성공 )", content = { @Content(mediaType = "application/json", schema = @Schema(implementation = BaseRes.class)) }) })
     @RequestMapping(method = RequestMethod.GET, value = "/read/{idx}")
-    public ResponseEntity read(@PathVariable Integer idx){
+    public ResponseEntity read(@PathVariable @NotNull @Positive Integer idx){
         User user = ((User)SecurityContextHolder.getContext().getAuthentication().getPrincipal());
         BaseRes baseRes = ordersService.read(user, idx);
 
         return ResponseEntity.ok().body(baseRes);
     }
 
+    @ApiOperation(value = "주문 취소", response = BaseRes.class, notes = "회원이 주문을 취소합니다. (상품 배송 전)")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK ( 요청 성공 )", content = { @Content(mediaType = "application/json", schema = @Schema(implementation = BaseRes.class)) }) })
     @RequestMapping(method = RequestMethod.DELETE, value = "/cancle/{idx}")
-    public ResponseEntity delete(@PathVariable Integer idx) {
+    public ResponseEntity delete(@PathVariable @NotNull @Positive Integer idx) {
         BaseRes baseRes = ordersService.delete(idx);
         return ResponseEntity.ok().body(baseRes);
     }
--- src/main/java/com/example/lonua/orders/model/request/PostCreateOrdersReq.java
+++ src/main/java/com/example/lonua/orders/model/request/PostCreateOrdersReq.java
@@ -1,7 +1,12 @@
 package com.example.lonua.orders.model.request;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.*;
+import org.hibernate.validator.constraints.Length;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import java.util.List;
 
 @Getter
@@ -11,11 +16,35 @@ import java.util.List;
 @Builder
 public class PostCreateOrdersReq {
 
+    @NotNull
+    @Min(1)
+    @Pattern(regexp = "^[1-9]\\d*$")
+    @ApiModelProperty(value = "총 주문 금액", example = "50000", required = true)
     private Integer amount;
+
+    @NotNull
+    @Pattern(regexp = "^imp_\\d{12}$")
+    @ApiModelProperty(value = "결제 번호", example = "imp_581455476041", required = true)
     private String impUid;
+
+    @NotNull
+    @Pattern(regexp = "^2024(0[1-9]|1[0-2])(0[1-9]|[12][0-9]|3[01])\\d{10}$")
+    @ApiModelProperty(value = "주문 번호", example = "202401062125264623", required = true)
     private String merchantUid;
+
+    @NotNull
+    @Length(max=45)
+    @ApiModelProperty(value = "상품명(45자 이하)", example = "하프 집업 카라넥 케이블 니트 [카키]", required = true)
     private String productName;
+
+    @NotNull
+    @Min(1)
+    @ApiModelProperty(value = "결제 수단", example = "카카오페이", required = true)
     private String payMethod;
+
+    @NotNull
+    @Pattern(regexp = "^\\[(\\d+(?:,\\s*\\d+)*)?\\]$")
+    @ApiModelProperty(value = "주문 상품 리스트", example = "[1, 2, 3]", required = true)
     private List<Integer> productIdxList;
 
 }
