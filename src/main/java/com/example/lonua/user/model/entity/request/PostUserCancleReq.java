bc72ff1ca584419bc53cc4a56bced6acfd057fe3
dk_Edu <pivottocoding@gmail.com>
2024-01-15 17:33:15 +0900

    [Feat]회원 탈퇴 처리 기능 추가 회원이 탈퇴 신청을 할 때 Status를 0으로 수정

---
+++ src/main/java/com/example/lonua/user/model/entity/request/PostUserCancleReq.java
@@ -0,0 +1,12 @@
+package com.example.lonua.user.model.entity.request;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class PostUserCancleReq {
+    private Integer userIdx;
+}
