f90fc933a35aa59b555b1a3a0abdd10a11fccc05
thanks9807 <thanks9807@naver.com>
2024-01-15 21:25:45 +0900

    [ERROR] checkout error

--- src/main/java/com/example/lonua/branch/controller/BranchController.java
+++ src/main/java/com/example/lonua/branch/controller/BranchController.java
@@ -21,6 +21,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.swing.text.html.parser.Entity;
 import javax.validation.Valid;
 
 @RestController
