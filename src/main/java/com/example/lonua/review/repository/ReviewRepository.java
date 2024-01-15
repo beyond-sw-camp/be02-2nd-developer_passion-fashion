a99f4800fe58c499717628ece9236d4964f822f8
jwj3 <llongkate@naver.com>
2024-01-15 17:59:27 +0900

    [FEAT] 리뷰 기능 추가 리뷰 기능을 추가했습니다

--- src/main/java/com/example/lonua/review/repository/ReviewRepository.java
+++ src/main/java/com/example/lonua/review/repository/ReviewRepository.java
@@ -3,8 +3,10 @@ package com.example.lonua.review.repository;
 import com.example.lonua.review.model.entity.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ReviewRepository extends JpaRepository<Review, Integer> {
     Optional<Review> findByReviewIdx(Integer reviewIdx);
+    List<Review> findByProduct_ProductIdx(Integer productIdx);
 }
