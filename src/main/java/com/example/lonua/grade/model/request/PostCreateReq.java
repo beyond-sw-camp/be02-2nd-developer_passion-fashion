f3e7b289227b35c9b399e9126919b72fb6baa97e
dk <pivottocoding@gmail.com>
2024-01-15 22:03:26 +0900

    [Feat] Swagger 및 Valid 기능 추가 Swagger 및 입력값 검증 기능 추가

--- src/main/java/com/example/lonua/grade/model/request/PostCreateReq.java
+++ src/main/java/com/example/lonua/grade/model/request/PostCreateReq.java
@@ -2,7 +2,10 @@ package com.example.lonua.grade.model.request;
 
 
 import com.example.lonua.user.model.entity.User;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.*;
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.Range;
 
 import javax.persistence.Column;
 import javax.persistence.OneToMany;
@@ -15,6 +18,11 @@ import java.util.List;
 @AllArgsConstructor
 @Builder
 public class PostCreateReq {
+    @Length(max=20)
+    @ApiModelProperty(value = "등록할 등급의 이름", example = "골드", required = true)
     private String gradeType;
+
+    @Range(min = 1, max = 10)
+    @ApiModelProperty(value = "등급의 할인율", example = "5", required = true)
     private Integer discountRate;
 }
