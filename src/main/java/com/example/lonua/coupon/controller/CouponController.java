713f29110fc1c61331f56f711936ff07b347ba94
dk <pivottocoding@gmail.com>
2024-01-15 22:15:12 +0900

    [Feat] Swagger 및 Valid 기능 추가 Swagger 및 입력값 검증 기능 추가

--- src/main/java/com/example/lonua/coupon/controller/CouponController.java
+++ src/main/java/com/example/lonua/coupon/controller/CouponController.java
@@ -4,12 +4,15 @@ import com.example.lonua.coupon.model.request.DeleteRemoveReq;
 import com.example.lonua.coupon.model.request.GetReadReq;
 import com.example.lonua.coupon.model.request.PostRegisterReq;
 import com.example.lonua.coupon.service.CouponService;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,22 +21,28 @@ public class CouponController {
 
     private final CouponService couponService;
 
+
+    @ApiOperation(value = "쿠폰 등록하기",httpMethod = "POST")
     @RequestMapping(method = RequestMethod.POST, value = "/register")
-    ResponseEntity registerCoupon(@RequestBody PostRegisterReq request) {
+    ResponseEntity registerCoupon(@RequestBody @Valid PostRegisterReq request) {
         return ResponseEntity.ok().body(couponService.create(request));
     }
 
+    @ApiOperation(value = "쿠폰 목록 가져오기")
     @RequestMapping(method = RequestMethod.GET, value = "/list")
     ResponseEntity listCoupon() {
         return ResponseEntity.ok().body(couponService.list());
     }
+
+    @ApiOperation(value = "쿠폰 하나 가져오기")
     @RequestMapping(method = RequestMethod.GET, value = "/read")
-    ResponseEntity readCoupon(@RequestBody GetReadReq request) {
+    ResponseEntity readCoupon(@RequestBody @Valid GetReadReq request) {
         return ResponseEntity.ok().body(couponService.read(request));
     }
 
+    @ApiOperation(value = "쿠폰 하나 삭제하기")
     @RequestMapping(method = RequestMethod.DELETE, value = "/delete")
-    ResponseEntity deleteCoupon(@RequestBody DeleteRemoveReq request) {
+    ResponseEntity deleteCoupon(@RequestBody @Valid DeleteRemoveReq request) {
         return ResponseEntity.ok().body(couponService.delete(request));
     }
 
--- src/main/java/com/example/lonua/coupon/model/request/PostRegisterReq.java
+++ src/main/java/com/example/lonua/coupon/model/request/PostRegisterReq.java
@@ -1,15 +1,19 @@
 package com.example.lonua.coupon.model.request;
 
 import com.example.lonua.user.model.entity.User;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Range;
 
 import javax.persistence.Column;
 import javax.persistence.FetchType;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Pattern;
 import java.util.Date;
 
 @Data
@@ -17,8 +21,19 @@ import java.util.Date;
 @NoArgsConstructor
 @AllArgsConstructor
 public class PostRegisterReq {
+    @Min(value = 1)
+    @ApiModelProperty(value = "쿠폰을 받을 사람의 Idx", example = "1", required = true)
     private Integer userIdx;
+
+    @Pattern(regexp = "^.{1,20}$", message = "20글자 이하의 문자열을 입력하세요.")
+    @ApiModelProperty(value = "발급할 쿠폰의 이름", example = "1", required = true)
     private String couponName;
+
+    @Range(min = 5,max= 100)
+    @ApiModelProperty(value = "발급할 쿠폰의 할인율", example = "50", required = true)
     private Integer couponDiscountRate;
-    private Boolean status;
+
+
+    @ApiModelProperty(value = "발급할 쿠폰의 상태", example = "false", required = true)
+    private Boolean status;  // 필요있나?
 }
