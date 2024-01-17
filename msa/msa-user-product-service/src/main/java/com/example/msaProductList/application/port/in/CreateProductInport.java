b18818585de02c6b23c28c920b8199b51aa85089
thanks9807 <thanks9807@naver.com>
2024-01-17 16:43:42 +0900

    [Docs] ãyml ëfile 환경변수 처리

--- msa/msa-user-product-service/src/main/java/com/example/msaProductList/application/port/in/CreateProductInport.java
+++
@@ -1,5 +0,0 @@
-package com.example.msaProductList.application.port.in;
-
-public interface CreateProductInport {
-    public void create(CreateProductCommand createProductCommand);
-}
--- msa/msa-user-product-service/src/main/java/com/example/msaProductList/application/port/in/ListProductInPort.java
+++
@@ -1,9 +0,0 @@
-package com.example.msaProductList.application.port.in;
-
-import com.example.msaProductList.domain.Product;
-
-import java.util.List;
-
-public interface ListProductInPort {
-    List<Product> list(Integer page, Integer size);
-}
