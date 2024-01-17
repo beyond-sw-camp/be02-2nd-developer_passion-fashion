3c7bb81872fa9f0e7c2d3393bcfd7d8544e67d9b
dk_Edu <pivottocoding@gmail.com>
2024-01-17 16:14:59 +0900

    [Fix] Cart list 조회 N+1 문제 해결 N+1 문제 해결

--- src/main/java/com/example/lonua/cart/repository/querydsl/CartRepositoryCustomImpl.java
+++ src/main/java/com/example/lonua/cart/repository/querydsl/CartRepositoryCustomImpl.java
@@ -3,7 +3,10 @@ package com.example.lonua.cart.repository.querydsl;
 import com.example.lonua.cart.model.entity.Cart;
 import com.example.lonua.cart.model.entity.QCart;
 
+import com.example.lonua.grade.model.entity.QGrade;
+import com.example.lonua.orders.model.entity.QOrders;
 import com.example.lonua.product.model.entity.QProduct;
+import com.example.lonua.product.model.entity.QProductCount;
 import com.example.lonua.user.model.entity.QUser;
 
 import com.querydsl.core.Tuple;
@@ -27,20 +30,21 @@ public class CartRepositoryCustomImpl extends QuerydslRepositorySupport implemen
         QCart cart = new QCart("cart");
         QProduct product = new QProduct("product");
         QUser user = new QUser("user");
+        QProductCount productCount = new QProductCount("productCount");
+        QOrders orders = new QOrders("orders");
 
 
         List<Cart> result = from(cart)
                 .leftJoin(cart.product, product).fetchJoin()
                 .leftJoin(cart.user, user).fetchJoin()
+                .leftJoin(product.productCount, productCount).fetchJoin()
+                .leftJoin(user.ordersList, orders).fetchJoin()
                 .where(user.userIdx.eq(userIdx))
                 .distinct()
                 .offset(pageable.getOffset())
                 .limit(pageable.getPageSize())
                 .fetch();
 
-
-
         return new PageImpl<>(result, pageable, result.size());
     }
-
 }
